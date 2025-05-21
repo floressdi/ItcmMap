781b885551bf45a0f7ee9bbdf7483f4eb0518326
unknown <fioriasus>
2025-05-21 14:38:55 -0600

    primer commit

---
+++ pages/ModelCampus1.js
@@ -0,0 +1,13 @@
+import { useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+export default function ModelCampus1() {
+  const gltf = useLoader(GLTFLoader, '/campus1/campusUnoItcm.glb');
+  gltf.scene.traverse((child) => {
+    if (child.isMesh) {
+        console.log(child.material);
+    }
+});
+
+  return <primitive object={gltf.scene}/>;
+}
